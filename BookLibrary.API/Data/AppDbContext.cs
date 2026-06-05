b24f2e89019373d95a9b5137f81a3c5a72a2af39
E-bot-rgb <zebastianericsson@gmail.com>
2026-06-05 21:27:55 +0200

    feat: add models and DbContext

---
+++ BookLibrary.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using BookLibrary.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibrary.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookLibrary.API/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookLibrary.API.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; } = null!;
+    }
+}
